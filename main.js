1c929bdc25050b6a896ed35c6ef7788b3df56ec2
hartaj-design <81467452+hartaj-design@users.noreply.github.com>
2022-07-13 19:52:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function bedroom(){
+    window.location="bd.html";
+}
+function kitchen(){
+    window.location="k.html";
+}
+function library(){
+    window.location="l.html";
+}
+function livingroom(){
+    window.location="lr.html";
+} 
+function desk(){
+    window.location="d.html";
+}
+function back(){
+    window.location="index.html";
+}
